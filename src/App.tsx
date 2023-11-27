d685e23e371916e1a7f40f9ccd6044f898d90466
Benoit Petit <petitbenoit3@gmail.com>
2023-11-27 17:45:57 +0100

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import Video from './assets/video.mp4'
 import './main.scss'
 
 const App = () => {
-  return <>watch video</>
+  return (
+    <>
+      <header>
+        <a
+          href="https://www.benjamingeffroy.com/la-plante-sauvage"
+          target="_blank"
+        >
+          Benjamin Geffroy
+        </a>
+      </header>
+      <main>
+        <section className="left">
+          <h1>La planète sauvage</h1>
+          <span>Direction & Animation: Benjamin Geffroy</span>
+        </section>
+        <div className="video-container">
+          <video crossOrigin="anonymous">
+            <source src={Video} type="video/mp4" />
+          </video>
+        </div>
+      </main>
+    </>
+  )
 }
 
 export default App
